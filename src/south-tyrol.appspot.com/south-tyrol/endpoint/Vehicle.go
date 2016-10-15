3450f178798d1f9b19113ddc8eeef66bdb5dea74
dejdav <dejoridavid@gmail.com>
2016-10-15 16:33:21 +0200

    Compacted endpoints

--- src/south-tyrol.appspot.com/south-tyrol/endpoint/Vehicle.go
+++ src/south-tyrol.appspot.com/south-tyrol/endpoint/Vehicle.go
@@ -7,10 +7,50 @@ import (
     "issue"
     "encoding/json"
     "appengine"
+    "model/sasa"
+    "strconv"
+    "strings"
 )
 
 type Car struct {
     Router *httprouter.Router
+}
+
+func (*Car) GetAvailable(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+    w.Header().Set("Content-Type", "application/json")
+
+    response := sasa.ReadJsonFromUrl(w, r, "http://realtimetest.opensasa.info/positions")
+
+    projection := new(sasa.Buses)
+    json.NewDecoder(response.Body).Decode(projection)
+
+    features := projection.Features
+    buses := make([]sasa.RealtimeBus, len(features))
+
+    for key, bus := range features {
+        p := bus.Properties;
+
+        vehicleId, _ := strconv.Atoi(strings.Split(p.VehicleId, " ")[0]);
+        latitude, longitude := sasa.ToWgs84(bus.Geometry.Coordinates, 32)
+
+        buses[key] = sasa.RealtimeBus{
+            LineName: p.LineName,
+            BusStop: p.BusStopName,
+            HydrogenBus: vehicleId >= 428 && vehicleId <= 432,
+            TripId: p.TripId,
+            Latitude: latitude,
+            Longitude: longitude,
+        }
+    }
+
+    vehicles, _ := vehicle.GetAll(appengine.NewContext(r))
+
+    data, _ := json.Marshal(vehicle.Vehicles{
+        Vehicles: vehicles,
+        Buses: buses,
+    })
+
+    w.Write(data)
 }
 
 func (*Car) GetMy(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
