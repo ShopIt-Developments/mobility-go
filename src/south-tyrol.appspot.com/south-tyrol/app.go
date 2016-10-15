172ad2f95c8162826e55afa23b49e1af624be6d7
dejdav <dejoridavid@gmail.com>
2016-10-15 16:33:42 +0200

    Renamed function

--- src/south-tyrol.appspot.com/south-tyrol/app.go
+++ src/south-tyrol.appspot.com/south-tyrol/app.go
@@ -5,14 +5,21 @@ import (
 	"github.com/julienschmidt/httprouter"
 
 	"endpoint"
+    "encoding/json"
+    "strconv"
+    "strings"
+    "model/sasa"
+    "model/vehicle"
+    "appengine"
 )
 
 func init() {
 	router := httprouter.New()
 
+    router.GET("/mobility/vehicles/available", rt)
+
     c := endpoint.Car{Router: router}
-    c.Router.GET("/mobility/vehicles/available", c.GetAvailable)
-    c.Router.GET("/mobility/vehicles/my", c.GetMy)
+    c.Router.GET("/mobility/vehicles/my", c.GetOwn)
     c.Router.GET("/mobility/vehicles", c.GetAll)
     c.Router.GET("/mobility/vehicle/:vehicle_id", c.GetOne)
     c.Router.POST("/mobility/vehicle", c.Add)
@@ -26,4 +33,41 @@ func init() {
 	u.Router.DELETE("/user", u.Delete)
 
 	http.Handle("/", router)
+}
+
+func rt(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+    w.Header().Set("Content-Type", "application/json")
+
+    response := sasa.ReadJsonFromUrl(w, r, "http://realtimetest.opensasa.info/positions")
+
+    projection := new(sasa.Buses)
+    json.NewDecoder(response.Body).Decode(projection)
+
+    features := projection.Features
+    buses := make([]sasa.RealtimeBus, len(features))
+
+    for key, bus := range features {
+        p := bus.Properties;
+
+        vehicleId, _ := strconv.Atoi(strings.Split(p.VehicleId, " ")[0]);
+        latitude, longitude := sasa.ToWgs84(bus.Geometry.Coordinates, 32)
+
+        buses[key] = sasa.RealtimeBus{
+            LineName: p.LineName,
+            BusStop: p.BusStopName,
+            HydrogenBus: vehicleId >= 428 && vehicleId <= 432,
+            TripId: p.TripId,
+            Latitude: latitude,
+            Longitude: longitude,
+        }
+    }
+
+    vehicles, _ := vehicle.GetAll(appengine.NewContext(r))
+
+    data, _ := json.Marshal(vehicle.Vehicles{
+        Vehicles: vehicles,
+        Buses: buses,
+    })
+
+    w.Write(data)
 }
