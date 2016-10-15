cfeb71b440322d975a3ad7b177ae5916c94296e9
dejdav <dejoridavid@gmail.com>
2016-10-16 00:07:31 +0200

    Fixed bug

--- src/south-tyrol.appspot.com/south-tyrol/endpoint/Vehicle.go
+++ src/south-tyrol.appspot.com/south-tyrol/endpoint/Vehicle.go
@@ -85,6 +85,8 @@ func (*Vehicle) New(w http.ResponseWriter, r *http.Request, p httprouter.Params)
     entity, err := vehicle.New(appengine.NewContext(r), r.Body, network.Authorization(w, r))
     issue.Handle(w, err, http.StatusBadRequest)
 
+    entity.Available = true
+
     data, err := json.Marshal(entity)
     issue.Handle(w, err, http.StatusInternalServerError)
 
